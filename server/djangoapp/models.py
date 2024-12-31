700115d9fc4c2e1e37496b87005655ad1338e1fa
pruthviraj <pruthvirajm1718@gmail.com>
2024-12-31 06:13:22 -0500

    Register the carmake and carmodel with admin site

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,57 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return self.name  
 
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    name = models.CharField(max_length=100)  # Model name
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('COUPE', 'Coupe'),
+        ('HATCHBACK', 'Hatchback'),
+        ('CONVERTIBLE', 'Convertible'),
+    ]
+    type = models.CharField(max_length=20, choices=CAR_TYPES, default='SUV')  # Type of the car
+    year = models.IntegerField(
+        validators=[
+            MaxValueValidator(2024), 
+            MinValueValidator(1886)
+        ]
+    )
+    dealer_id = models.IntegerField()
+    fuel_type = models.CharField(
+        max_length=20,
+        choices=[
+            ('PETROL', 'Petrol'),
+            ('DIESEL', 'Diesel'),
+            ('ELECTRIC', 'Electric'),
+            ('HYBRID', 'Hybrid'),
+        ],
+        default='PETROL'
+    )
+    transmission = models.CharField(
+        max_length=20,
+        choices=[
+            ('MANUAL', 'Manual'),
+            ('AUTOMATIC', 'Automatic'),
+        ],
+        default='MANUAL'
+    )
+    color = models.CharField(max_length=50, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.type})"
