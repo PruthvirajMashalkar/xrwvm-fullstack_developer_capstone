0e8fe212ed4f43b0981bd191540ce59022ad9088
pruthviraj <pruthvirajm1718@gmail.com>
2025-01-01 19:58:19 +0530

    fixed lint error in python files

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -188,10 +188,11 @@ def initiate():
         }
     ]
 
-for data in car_model_data:
-    CarModel.objects.create(name=data['name'], car_make=data['car_make'],
-                            type=data['type'], year=data['year'],
-                            color=data['color'], price=data['price'],
-                            fuel_type=data['fuel_type'],
-                            transmission=data['transmission'],
-                            dealer_id=data['dealer_id'])
+
+    for data in car_model_data:
+        CarModel.objects.create(name=data['name'], car_make=data['car_make'],
+                                type=data['type'], year=data['year'],
+                                color=data['color'], price=data['price'],
+                                fuel_type=data['fuel_type'],
+                                transmission=data['transmission'],
+                                dealer_id=data['dealer_id'])
