e963a857c8265b472f914a64d97776dab67cf237
pruthviraj <pruthvirajm1718@gmail.com>
2025-01-01 16:28:56 +0530

    Fixed Lint errors in python files

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,21 +1,23 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 class CarMake(models.Model):
+    
     name = models.CharField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
-        return self.name  
+        return self.name
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
-    name = models.CharField(max_length=100)  # Model name
+
+    car_make = models.ForeignKey(CarMake,
+    on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
@@ -24,13 +26,14 @@ class CarModel(models.Model):
         ('HATCHBACK', 'Hatchback'),
         ('CONVERTIBLE', 'Convertible'),
     ]
-    type = models.CharField(max_length=20, choices=CAR_TYPES, default='SUV')  # Type of the car
+    type = models.CharField(max_length=20,
+    choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(
         validators=[
-            MaxValueValidator(2024), 
+            MaxValueValidator(2024),
             MinValueValidator(1886)
-        ]
-    )
+            ]
+        )
     dealer_id = models.IntegerField()
     fuel_type = models.CharField(
         max_length=20,
@@ -50,8 +53,10 @@ class CarModel(models.Model):
         ],
         default='MANUAL'
     )
-    color = models.CharField(max_length=50, blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    color = models.CharField(max_length=50,
+    blank=True, null=True)
+    price = models.DecimalField(max_digits=10,
+    decimal_places=2, blank=True, null=True)
 
     def __str__(self):
         return f"{self.car_make.name} {self.name} ({self.type})"
