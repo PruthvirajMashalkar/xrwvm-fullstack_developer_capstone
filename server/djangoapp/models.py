abd696b3cc4aca4f9f7863bcfe378a2f335758c8
pruthviraj <pruthvirajm1718@gmail.com>
2025-01-01 16:41:23 +0530

    Fixed Lint errors in python files

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -6,18 +6,17 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class CarMake(models.Model):
-    
     name = models.CharField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
         return self.name
 
-class CarModel(models.Model):
 
-    car_make = models.ForeignKey(CarMake,
-    on_delete=models.CASCADE)
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+    
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
@@ -26,15 +25,16 @@ class CarModel(models.Model):
         ('HATCHBACK', 'Hatchback'),
         ('CONVERTIBLE', 'Convertible'),
     ]
-    type = models.CharField(max_length=20,
-    choices=CAR_TYPES, default='SUV')
+    
+    type = models.CharField(max_length=20, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(
         validators=[
             MaxValueValidator(2024),
             MinValueValidator(1886)
-            ]
-        )
+        ]
+    )
     dealer_id = models.IntegerField()
+    
     fuel_type = models.CharField(
         max_length=20,
         choices=[
@@ -45,6 +45,7 @@ class CarModel(models.Model):
         ],
         default='PETROL'
     )
+    
     transmission = models.CharField(
         max_length=20,
         choices=[
@@ -53,10 +54,9 @@ class CarModel(models.Model):
         ],
         default='MANUAL'
     )
-    color = models.CharField(max_length=50,
-    blank=True, null=True)
-    price = models.DecimalField(max_digits=10,
-    decimal_places=2, blank=True, null=True)
+    
+    color = models.CharField(max_length=50, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
         return f"{self.car_make.name} {self.name} ({self.type})"
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,15 +1,11 @@
-# Uncomment the required imports before adding the code
-
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
 from django.contrib import messages
 from datetime import datetime
 from .models import CarMake, CarModel
-from django.http import JsonResponse
-from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
@@ -18,7 +14,6 @@ from .restapis import get_request, analyze_review_sentiments, post_review
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
-
 
 # Create your views here.
 
@@ -29,7 +24,7 @@ def login_user(request):
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
-    # Try to check if provide credential can be authenticated
+    # Try to check if provided credentials can be authenticated
     user = authenticate(username=username, password=password)
     data = {"userName": username}
     if user is not None:
@@ -51,8 +46,6 @@ def logout_user(request):
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    context = {}
-
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
@@ -82,7 +75,6 @@ def registration(request):
 
 # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
-# Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
     if state == "All":
         endpoint = "/fetchDealers"
@@ -93,13 +85,11 @@ def get_dealerships(request, state="All"):
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
-    # if dealer id has been provided
     if dealer_id:
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
-            print(response)
             review_detail['sentiment'] = response['sentiment']
         return JsonResponse({"status": 200, "reviews": reviews})
     else:
@@ -119,7 +109,7 @@ def add_review(request):
     if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({"status": 401, "message": "Error in posting review"})
@@ -128,11 +118,8 @@ def add_review(request):
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
-    print(count)
     if count == 0:
         initiate()
     car_models = CarModel.objects.select_related('car_make')
-    cars = []
-    for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    cars = [{"CarModel": car_model.name, "CarMake": car_model.car_make.name} for car_model in car_models]
     return JsonResponse({"CarModels": cars})
